e9a79b93902eb98bc53944cb1e8d05796cfc0553
Mateusz Kita <mkita.zg@gmail.com>
2018-05-16 20:09:02 +0200

    using table to json

--- mainScraper.js
+++ mainScraper.js
@@ -1,9 +1,19 @@
 const cheerio = require('cheerio')
 const request = require('request')
+const tableToJson = require('tabletojson')
+
 
 const roomsUrl = 'http://www.plan.uz.zgora.pl/sale_lista_budynkow.php?pTyp=P'
 
 
 request(roomsUrl, (error, response) => {
-    console.log(response.body)
+    if (!error) {
+        let tables = tableToJson.convert(response.body)
+        console.log(tables[0])
+    }
+    else {
+        console.log(error)
+    }
 })
+
+
