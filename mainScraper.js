894b0c86a924eaafdabe436d61a348f37b6f1227
Mateusz Kita <mkita.zg@gmail.com>
2018-05-16 20:22:18 +0200

    using cheerio to scrap a href

--- mainScraper.js
+++ mainScraper.js
@@ -8,8 +8,13 @@ const roomsUrl = 'http://www.plan.uz.zgora.pl/sale_lista_budynkow.php?pTyp=P'
 
 request(roomsUrl, (error, response) => {
     if (!error) {
-        let tables = tableToJson.convert(response.body)
-        console.log(tables[0])
+        // let tables = tableToJson.convert(response.body)
+        // console.log(tables[0])
+
+        const $ = cheerio.load(response.body);
+        $('tr td a').each((index, el) => {
+            console.log(el)
+        })
     }
     else {
         console.log(error)
