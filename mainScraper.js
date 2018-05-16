9621aa756cba8671de68faddf1b0905ec0726415
Mateusz Kita <mkita.zg@gmail.com>
2018-05-16 19:51:50 +0200

    basic request added

--- mainScraper.js
+++ mainScraper.js
@@ -1,3 +1,9 @@
-const cheerio = require('cherrio')
+const cheerio = require('cheerio')
 const request = require('request')
 
+const roomsUrl = 'http://www.plan.uz.zgora.pl/sale_lista_budynkow.php?pTyp=P'
+
+
+request(roomsUrl, (error, response) => {
+    console.log(response.body)
+})
